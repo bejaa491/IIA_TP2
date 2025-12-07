a7b03253d2dd98b03112ea262048fee2d1c85b21
Gilberto16945 <iscac16945@alumni.iscac.pt>
2025-12-07 19:02:26 +0000

    Add utility functions and problem data structure for distance calculations
    - Implemented `init_data` function to read problem data from a file, initializing the distance matrix.
    - Added random number generation utilities: `init_rand`, `flip`, `random_l_h`, and `rand_01`.
    - Defined a `Problem` struct to hold the number of candidates and their distance matrix.
    - Created header file `utils.h` to declare utility functions and the `Problem` struct.

---
+++ src_gmr/funcao.h
@@ -0,0 +1,17 @@
+#ifndef FUNCAO_H
+#define FUNCAO_H
+
+#include "utils.h"   // para MAX_CANDIDATES e Problem/prob
+
+typedef struct {
+    int selected[MAX_CANDIDATES];  // 1 se o ponto i está escolhido
+    int num_selected;              // quantos pontos estão a 1
+    double fitness;                // valor da função objetivo
+} Solution;
+
+double calculate_fitness(Solution *s);
+void random_solution(Solution *s);
+void copy_solution(Solution *dest, const Solution *src);
+void print_solution(const Solution *s);
+
+#endif
---
+++ src_gmr/utils.h
@@ -0,0 +1,23 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#define MAX_CANDIDATES 500
+
+typedef struct {
+    int C;
+    int m;
+    double dist[MAX_CANDIDATES][MAX_CANDIDATES];
+} Problem;
+
+extern Problem prob;
+
+// Leitura dos dados do problema
+int init_data(const char *filename);
+
+// Funções utilitárias
+void init_rand(void);
+int  flip(void);
+int  random_l_h(int min, int max);
+float rand_01(void);
+
+#endif
